3b3e8c608aedd6bb00004d9d7d28750ef6dbf584
Yunji <zion830@naver.com>
2019-09-10 15:30:51 +0900

    programmers 스킬트리 수정

--- programmers/level2/스킬트리/Solution.java
+++ programmers/level2/스킬트리/Solution.java
@@ -1,4 +1,4 @@
-class Solution {
+﻿class Solution {
 
     public int solution(String skill, String[] skillTrees) {
         int answer = 0;
@@ -14,19 +14,15 @@ class Solution {
 
     private boolean isAvailable(String skill, String skillTree) {
         int order = 0; // 학습 순서
-        char nextStep = skill.charAt(order); // 다음에 배워야 할 스킬
 
         for (int i = 0; i < skillTree.length(); i++) {
-            for (int j = 0; j < skill.length(); j++) {
-                char selectedSkill = skill.charAt(j);
+            char selectedSkill = skillTree.charAt(i);
 
-                if (skillTree.charAt(i) == selectedSkill) {
-                    if (selectedSkill != nextStep) {
-                        return false;
-                    } else if (order < skill.length() - 1) {
-                        order++;
-                        nextStep = skill.charAt(order);
-                    }
+            if (skill.contains(String.valueOf(selectedSkill))) {
+                if (selectedSkill == skill.charAt(order)) {
+                    order++;
+                } else {
+                    return false;
                 }
             }
         }
